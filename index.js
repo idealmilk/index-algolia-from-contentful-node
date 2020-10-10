e96c6389c326257ee71c85086817955e9d924c21
Matthew Gilligan <mcgilligan97@gmail.com>
2020-10-10 19:02:00 +0100

    Index artist, feature, news as well

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ require("dotenv").config()
     accessToken,
   })
 
+  algoliaIndex.clearObjects()
+
   try {
     const { items } = await ctfClient.getEntries({
       content_type: "review",
@@ -31,6 +33,7 @@ require("dotenv").config()
       artist: review.fields.artist,
       author: review.fields.author,
       albumCover: review.fields.albumCover,
+      publishedDate: review.fields.publishedDate,
       objectID: review.sys.id,
     }))
 
@@ -40,4 +43,70 @@ require("dotenv").config()
   } catch (err) {
     console.error(err)
   }
+
+  // try {
+  //   const { items } = await ctfClient.getEntries({
+  //     content_type: "news",
+  //     limit: 1000,
+  //   })
+  //   const news = items.map((n) => ({
+  //     slug: n.fields.slug,
+  //     title: n.fields.title,
+  //     artist: n.fields.artist,
+  //     author: n.fields.author,
+  //     coverImage: n.fields.albumCover,
+  //     publishedDate: n.fields.publishedDate,
+  //     objectID: n.sys.id,
+  //   }))
+
+  //   const indexedContent = await algoliaIndex.saveObjects(news, true)
+
+  //   console.log("Indexed Content:", indexedContent)
+  // } catch (err) {
+  //   console.error(err)
+  // }
+
+  // try {
+  //   const { items } = await ctfClient.getEntries({
+  //     content_type: "feature",
+  //     limit: 1000,
+  //   })
+  //   const features = items.map((feature) => ({
+  //     slug: feature.fields.slug,
+  //     title: feature.fields.title,
+  //     subtitle: feature.fields.subtitle,
+  //     artist: feature.fields.artist,
+  //     author: feature.fields.author,
+  //     coverImage: feature.fields.coverImage,
+  //     publishedDate: feature.fields.publishedDate,
+  //     objectID: feature.sys.id,
+  //   }))
+
+  //   const indexedContent = await algoliaIndex.saveObjects(features, true)
+
+  //   console.log("Indexed Content:", indexedContent)
+  // } catch (err) {
+  //   console.error(err)
+  // }
+
+  // try {
+  //   const { items } = await ctfClient.getEntries({
+  //     content_type: "artist",
+  //     limit: 1000,
+  //   })
+  //   const artists = items.map((artist) => ({
+  //     slug: artist.fields.slug,
+  //     englishName: artist.fields.englishName,
+  //     japaneseName: artist.fields.japaneseName,
+  //     objectID: artist.sys.id,
+  //   }))
+
+  //   const indexedContent = await algoliaIndex.saveObjects(artists, true)
+
+  //   console.log("Indexed Content:", indexedContent)
+  // } catch (err) {
+  //   console.error(err)
+  // }
+
+
 })()
